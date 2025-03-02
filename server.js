cd6177ba1a5347e320670c0d0e938c6a1395cce3
Phoovich <Phoovich@gmail.com>
2025-03-02 22:37:00 +0700

    change port

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const cookieParser = require('cookie-parser');
-const connectDB = require('./config/db');
+const express = require("express");
+const dotenv = require("dotenv");
+const cookieParser = require("cookie-parser");
+const connectDB = require("./config/db");
 
 // Load env vars
-dotenv.config({path:'./config/config.env'});
+dotenv.config({ path: "./config/config.env" });
 
 //connect to database
 connectDB();
@@ -18,15 +18,23 @@ app.use(express.json());
 app.use(cookieParser());
 
 //Mount routers
-app.use
+app.use;
 
-
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, console.log('Server running in ', process.env.NODE_ENV, ' mode on port', PORT));
+const PORT = process.env.PORT || 5003;
+app.listen(
+  PORT,
+  console.log(
+    "Server running in ",
+    process.env.NODE_ENV,
+    " mode on port",
+    PORT,
+  ),
+);
 
 //Handle unhandled promise rejections
-process.on('unhandledRejection', (err, promise) => {
-    console.log(`Error : ${err.message}`);
-    //close server & exit process
-    server.close(() => process.exit(1));
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error : ${err.message}`);
+  //close server & exit process
+  server.close(() => process.exit(1));
 });
+
