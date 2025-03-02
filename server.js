2c991291b6b8e51d3a48c6bbe8d7f0f167e842e6
Phoovich <Phoovich@gmail.com>
2025-03-02 22:30:32 +0700

    fix server.js

--- server.js
+++ server.js
@@ -17,24 +17,9 @@ app.use(express.json());
 //Cookie parser
 app.use(cookieParser());
 
-//route files
-const hospitals = require('./routes/hospitals');
-const auth = require('./routes/auth');
-const appointments = require('./routes/appointments');
-
 //Mount routers
-app.use('/api/v1/hospitals', hospitals);
-app.use('/api/v1/auth', auth);
-app.use('/api/v1/appointments', appointments);
+app.use
 
-// app.get('/', (req, res) => {
-//     //1. res.send('<h1>Hello from express</h1>');
-//     //2. res.send({name:'Brad'});
-//     //3. res.json({name:'Brad'});
-//     //4. res.sendStatus(400);
-//     //5. res.status(400).json({success:false});
-//     res.status(200).json({success:true, data:{id:1}});
-// });
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, console.log('Server running in ', process.env.NODE_ENV, ' mode on port', PORT));
