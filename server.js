3700c4dc390bf66fb902b0c2d0d9f560b2822dbd
Phoovich <Phoovich@gmail.com>
2025-03-24 14:08:07 +0700

    fix vercel

--- server.js
+++ server.js
@@ -3,42 +3,27 @@ const dotenv = require("dotenv");
 const cookieParser = require("cookie-parser");
 const connectDB = require("./config/db");
 
-//Load env vara
+// Load env variables
 dotenv.config({ path: "./config/config.env" });
 
-//ConnectDB();
+// Connect to database
 connectDB();
 
-//Route files
+// Route files
 const campgrouds = require("./routes/campgrounds");
 const bookings = require("./routes/bookings");
 const auth = require("./routes/auth");
 
 const app = express();
 
-//Body parser
+// Body parser
 app.use(express.json());
 app.use(cookieParser());
 
+// Routes
 app.use("/api/v1/campgrounds", campgrouds);
 app.use("/api/v1/auth", auth);
 app.use("/api/v1/bookings", bookings);
 
-const PORT = process.env.PORT || 5003;
-
-const server = app.listen(
-  PORT,
-  console.log(
-    "Server running in ",
-    process.env.NODE_ENV,
-    " mode on port ",
-    PORT,
-  ),
-);
-
-//Handle unhandled promise rejections
-process.on("unhandledRejection", (err, promise) => {
-  console.log(`Error: ${err.message}`);
-  //close server & exit process
-  server.close(() => process.exit(1));
-});
+// Export app for Vercel
+module.exports = app;
